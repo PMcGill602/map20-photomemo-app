2c2ef04551e8b1f423d2cc437fda7ba0f5a5d9de
Patrick McGill <mcgillpatrick60@gmail.com>
2020-10-06 19:22:38 -0500

    init

---
+++ android/app/src/main/kotlin/edu/uco/pmcgill/map2020/photomemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.uco.pmcgill.map2020.photomemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PhotoMemoApp());
+}
+
+class PhotoMemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('hekk'),
+    );
+  }
+
+}
